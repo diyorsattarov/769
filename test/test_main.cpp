ef4bbb4aaf1997d9506d2cc6bc291cae24841923
diyooor <diyorsattarov7@gmail.com>
2023-08-27 18:05:41 -0500

    Qt nested layout example

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,5 +1,30 @@
-#include <iostream>
+#include <QApplication>
+#include <QtWidgets>
+#include <QMainWindow>
 
-int main() {
-	std::cout << "helloworld" << std::endl;
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QWidget window;
+
+    QLabel *queryLabel = new QLabel(QApplication::translate("nestedlayouts", "Query:"));
+    QLineEdit *queryEdit = new QLineEdit();
+    QTableView *resultView = new QTableView();
+
+    QHBoxLayout *queryLayout = new QHBoxLayout();
+    queryLayout->addWidget(queryLabel);
+    queryLayout->addWidget(queryEdit);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout();
+    mainLayout->addLayout(queryLayout);
+    mainLayout->addWidget(resultView);
+    window.setLayout(mainLayout);
+    
+
+    window.setWindowTitle(
+        QApplication::translate("nestedlayouts", "Nested layout"));
+    
+    window.show(); // Show the window
+
+    return app.exec(); // Start the Qt event loop
 }
