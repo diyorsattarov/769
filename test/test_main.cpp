797e483251540a77d0d893a52a3fef74bf112c8e
diyor <diyorsattarov7@gmail.com>
2023-09-04 22:18:55 -0500

    Piece selection -- - Currently the piece selection has a bug, each side can select one of their back row and all of the pawns - need to implement control for each side as only one side can select a piece at a time

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,66 @@
 #include <QApplication>
 #include <QtWidgets>
 
+// Define a class for chess squares
+class ChessSquare : public QLabel {
+public:
+    ChessSquare(bool isBrown) {
+        setFixedSize(50, 50);
+        if (isBrown) {
+            setStyleSheet("background-color: #D2B48C;");
+        } else {
+            setStyleSheet("background-color: #8B4513;");
+        }
+    }
+};
+
+// Define a class for chess pieces
+class ChessPiece : public QLabel {
+    Q_OBJECT
+
+public:
+    ChessPiece(const QString& text, bool isWhite) : isWhite(isWhite), isSelected(false) {
+        setText(text);
+        setAlignment(Qt::AlignCenter);
+        setFixedSize(50, 50);
+        updateStyle(); // Initialize style
+    }
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override {
+        // Handle mouse click event
+        if (!isSelected) {
+            setSelected(true);
+            emit pieceSelected(this);
+        }
+    }
+
+public slots:
+    void setSelected(bool selected) {
+        isSelected = selected;
+        updateStyle(); // Update style when selected/unselected
+    }
+
+signals:
+    void pieceSelected(ChessPiece* selectedPiece);
+
+private:
+    bool isWhite;
+    bool isSelected;
+
+    void updateStyle() {
+        if (isSelected) {
+            setStyleSheet("QLabel { color: white; background-color: yellow; font-weight: bold; border: 1px solid black; }");
+        } else {
+            if (isWhite) {
+                setStyleSheet("QLabel { color: white; background-color: transparent; font-weight: bold; border: 1px solid black; }");
+            } else {
+                setStyleSheet("QLabel { color: black; background-color: transparent; font-weight: bold; border: 1px solid white; }");
+            }
+        }
+    }
+};
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -8,54 +68,50 @@ int main(int argc, char *argv[])
 
     QGridLayout *chessboardLayout = new QGridLayout;
 
-    // Create QLabel widgets for the chessboard squares
-    QLabel *squares[8][8];
+    // Create ChessSquare and ChessPiece objects
+    ChessSquare* squares[8][8];
+    ChessPiece* pieces[8][8];
+    ChessPiece* selectedBlackPiece = nullptr; // Store the currently selected black piece
+    ChessPiece* selectedWhitePiece = nullptr; // Store the currently selected white piece
+
     for (int row = 0; row < 8; ++row) {
         for (int col = 0; col < 8; ++col) {
-            squares[row][col] = new QLabel();
             bool isBrownSquare = (row + col) % 2 == 0;
-            if (isBrownSquare) {
-                squares[row][col]->setStyleSheet("background-color: #D2B48C;"); // Light brown for brown squares
-            } else {
-                squares[row][col]->setStyleSheet("background-color: #8B4513;"); // Brown for light brown squares
-            }
-            squares[row][col]->setFixedSize(50, 50);
-
+            squares[row][col] = new ChessSquare(isBrownSquare);
             chessboardLayout->addWidget(squares[row][col], row, col);
         }
     }
 
-    // Create QLabel widgets for chess pieces
     QString whitePieces[8] = {"R", "N", "B", "Q", "K", "B", "N", "R"};
     QString blackPieces[8] = {"r", "n", "b", "q", "k", "b", "n", "r"};
     QString whitePawns[8] = {"P", "P", "P", "P", "P", "P", "P", "P"};
     QString blackPawns[8] = {"p", "p", "p", "p", "p", "p", "p", "p"};
 
     for (int i = 0; i < 8; ++i) {
-        QLabel *whitePiece = new QLabel(whitePieces[i]);
-        QLabel *blackPiece = new QLabel(blackPieces[i]);
-        QLabel *whitePawn = new QLabel(whitePawns[i]);
-        QLabel *blackPawn = new QLabel(blackPawns[i]);
+        pieces[0][i] = new ChessPiece(blackPieces[i], false);
+        pieces[1][i] = new ChessPiece(blackPawns[i], false);
+        pieces[6][i] = new ChessPiece(whitePawns[i], true);
+        pieces[7][i] = new ChessPiece(whitePieces[i], true);
 
-        whitePiece->setStyleSheet("QLabel { color: white; background-color: transparent; font-weight: bold; border: 1px solid black; }");
-        blackPiece->setStyleSheet("QLabel { color: black; background-color: transparent; font-weight: bold; border: 1px solid white; }");
-        whitePawn->setStyleSheet("QLabel { color: white; background-color: transparent; font-weight: bold; border: 1px solid black; }");
-        blackPawn->setStyleSheet("QLabel { color: black; background-color: transparent; font-weight: bold; border: 1px solid white; }");
+        // Connect the pieceSelected signal to select/deselect pieces for black and white
+        QObject::connect(pieces[0][i], &ChessPiece::pieceSelected, [&](ChessPiece* selected) {
+            if (selectedBlackPiece) {
+                selectedBlackPiece->setSelected(false);
+            }
+            selectedBlackPiece = selected;
+        });
 
-        whitePiece->setAlignment(Qt::AlignCenter);
-        blackPiece->setAlignment(Qt::AlignCenter);
-        whitePawn->setAlignment(Qt::AlignCenter);
-        blackPawn->setAlignment(Qt::AlignCenter);
+        QObject::connect(pieces[7][i], &ChessPiece::pieceSelected, [&](ChessPiece* selected) {
+            if (selectedWhitePiece) {
+                selectedWhitePiece->setSelected(false);
+            }
+            selectedWhitePiece = selected;
+        });
 
-        whitePiece->setFixedSize(50, 50);
-        blackPiece->setFixedSize(50, 50);
-        whitePawn->setFixedSize(50, 50);
-        blackPawn->setFixedSize(50, 50);
-
-        chessboardLayout->addWidget(whitePiece, 7, i); // White pieces on the bottom
-        chessboardLayout->addWidget(blackPiece, 0, i); // Black pieces on the top
-        chessboardLayout->addWidget(whitePawn, 6, i); // White pawns on the second row from the bottom
-        chessboardLayout->addWidget(blackPawn, 1, i); // Black pawns on the second row from the top
+        chessboardLayout->addWidget(pieces[0][i], 0, i); // Black pieces on the top
+        chessboardLayout->addWidget(pieces[1][i], 1, i); // Black pawns on the second row from the top
+        chessboardLayout->addWidget(pieces[6][i], 6, i); // White pawns on the second row from the bottom
+        chessboardLayout->addWidget(pieces[7][i], 7, i); // White pieces on the bottom
     }
 
     window.setLayout(chessboardLayout);
@@ -64,3 +120,5 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+#include "test_main.moc" // Add this line
