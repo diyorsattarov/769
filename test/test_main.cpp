d09dba02c6b74ed3e03fa33f00d7ad75642e5a3a
diyor <diyorsattarov7@gmail.com>
2023-09-04 21:59:33 -0500

    Added white and black pieces to the chess board

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -25,8 +25,41 @@ int main(int argc, char *argv[])
         }
     }
 
+    // Create QLabel widgets for chess pieces
+    QString whitePieces[8] = {"R", "N", "B", "Q", "K", "B", "N", "R"};
+    QString blackPieces[8] = {"r", "n", "b", "q", "k", "b", "n", "r"};
+    QString whitePawns[8] = {"P", "P", "P", "P", "P", "P", "P", "P"};
+    QString blackPawns[8] = {"p", "p", "p", "p", "p", "p", "p", "p"};
+
+    for (int i = 0; i < 8; ++i) {
+        QLabel *whitePiece = new QLabel(whitePieces[i]);
+        QLabel *blackPiece = new QLabel(blackPieces[i]);
+        QLabel *whitePawn = new QLabel(whitePawns[i]);
+        QLabel *blackPawn = new QLabel(blackPawns[i]);
+
+        whitePiece->setStyleSheet("QLabel { color: white; background-color: transparent; font-weight: bold; border: 1px solid black; }");
+        blackPiece->setStyleSheet("QLabel { color: black; background-color: transparent; font-weight: bold; border: 1px solid white; }");
+        whitePawn->setStyleSheet("QLabel { color: white; background-color: transparent; font-weight: bold; border: 1px solid black; }");
+        blackPawn->setStyleSheet("QLabel { color: black; background-color: transparent; font-weight: bold; border: 1px solid white; }");
+
+        whitePiece->setAlignment(Qt::AlignCenter);
+        blackPiece->setAlignment(Qt::AlignCenter);
+        whitePawn->setAlignment(Qt::AlignCenter);
+        blackPawn->setAlignment(Qt::AlignCenter);
+
+        whitePiece->setFixedSize(50, 50);
+        blackPiece->setFixedSize(50, 50);
+        whitePawn->setFixedSize(50, 50);
+        blackPawn->setFixedSize(50, 50);
+
+        chessboardLayout->addWidget(whitePiece, 7, i); // White pieces on the bottom
+        chessboardLayout->addWidget(blackPiece, 0, i); // Black pieces on the top
+        chessboardLayout->addWidget(whitePawn, 6, i); // White pawns on the second row from the bottom
+        chessboardLayout->addWidget(blackPawn, 1, i); // Black pawns on the second row from the top
+    }
+
     window.setLayout(chessboardLayout);
-    window.setWindowTitle("Chessboard");
+    window.setWindowTitle("Chessboard with Pieces");
     window.show();
 
     return app.exec();
